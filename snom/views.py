ffb81d96443bee37d7ec199537701f8e9ff2d802
zopyx <yet@gmx.de>
2014-06-19 07:55:06 +0200

    added

---
+++ snom/views.py
@@ -0,0 +1,31 @@
+import requests
+import pprint
+from pyramid.response import Response
+from pyramid.view import view_config
+
+API_KEY = '74bd568f272457a41da25719fc69af8f'
+
+@view_config(route_name='home', renderer='templates/mytemplate.pt')
+def my_view(request):
+    return {'project': 'snom'}
+
+@view_config(name='incoming-call', renderer='templates/incoming-call.pt')
+def incoming_call(request):
+
+    number = request.params['number']
+    number = '06897768639'
+    params = dict(key=API_KEY, number=number)
+    result = requests.get('http://openapi.klicktel.de/searchapi/invers', params=params)
+    if result.status_code == 200:
+        result = result.json()
+        try:
+            entry = result['response']['results'][0]['entries'][0]
+        except IndexError:
+            return Response(status=404)
+
+        pprint.pprint(entry)
+        pprint.pprint(entry['displayname'])
+        return dict(entry=entry)
+
+    return Response(status=404)
+
